a0403a6021b712239b92852a4ff653568495eaa7
krystofsvaton <svatykr@gmail.com>
2022-05-20 09:02:08 +0200

    checkboxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
-import { Heading } from "../src/styles/layout-styles";
+import {Heading} from "../src/styles/layout-styles";
 import React, {ChangeEvent, useEffect, useState} from "react";
 import axios from "axios";
 import Caravan from "../src/components/caravans/caravan";
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import { Formik, Field } from "formik";
+import {Formik, Field} from "formik";
 import FormSection from "../src/components/filter/form-section";
 
 
@@ -37,6 +37,8 @@ const Home = () => {
         getCaravanData()
     }, [])
 
+    const caravans = ["Integrated", "Campervan", "BuiltIn", "Alcove"];
+
     return (
         <PageWrapper>
             <Heading>Prague Labs</Heading>
@@ -46,6 +48,7 @@ const Home = () => {
                         topRange: 0,
                         bottomRange: 2000,
                         instantBookable: "Ano",
+                        checked: []
                     }}
                     validateOnChange={false}
                     validateOnMount={false}
@@ -62,11 +65,11 @@ const Home = () => {
                                         defaultValue={[0, 2000]}
                                         step={20}
                                         value={[values.topRange, values.bottomRange]}
-                                        onChange={((value: any) =>{
+                                        onChange={((value: any) => {
                                             setFieldValue('topRange', value[0]);
                                             setFieldValue('bottomRange', value[1]);
                                             submitForm()
-                                        } )}
+                                        })}
                                     />
                                     <Field
                                         name="topRange"
@@ -74,7 +77,7 @@ const Home = () => {
                                         step={"20"}
                                         min={0}
                                         max={2000}
-                                        onChange={(e: ChangeEvent<HTMLInputElement>) =>{
+                                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
                                             handleChange(e)
                                             submitForm()
                                         }}
@@ -93,7 +96,52 @@ const Home = () => {
 
                                 </FormSection>
                                 <FormSection heading={"Typ karavanu"}>
-                                    ABC
+                                    <div>
+                                        {caravans.map((caravan) => {
+                                            return (
+                                                <Label key={caravan}>
+                                                    <CustomField
+                                                        type="checkbox"
+                                                        name="checked"
+                                                        value={caravan}
+                                                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                                            handleChange(e)
+                                                            submitForm()
+                                                        }}
+                                                    />
+                                                    {caravan}
+                                                </Label>
+                                            )
+                                        })}
+                                    </div>
+{/*
+                                    <Label key={caravan} id={caravan}>
+
+                                        <CustomField
+                                            name={caravan}
+                                            render={({ field, form }) => {
+                                                return (
+                                                    <input
+                                                        type="checkbox"
+                                                        id={caravan}
+                                                        checked={field.value}
+                                                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                                            setFieldValue(caravan, e.target.value);
+                                                            submitForm()
+                                                        }}
+                                                        {...field}
+                                                    />
+                                                );
+                                            }}
+                                        />
+                                        {caravan}
+                                    </Label>
+*/}
+
+
+
+
+
                                 </FormSection>
                                 <FormSection heading={"Okamžitá rezervace"}>
                                     <Field
@@ -146,5 +194,30 @@ const FormikForm = styled.div`
   width: 100%;
   display: flex;
 `
+
+const Label = styled.label`
+  border: 1px solid #EDEAE3;
+  cursor: pointer;
+  border-radius: 8px;
+  color: #9C8C8C;
+  font-size: 16px;
+  margin: 0 16px 16px 0;
+  padding: 20px;
+
+  :hover {
+    border: 2px solid #119383;
+  }
+
+  :active {
+    border-color: transparent;
+    background-color: #a4e0fd;
+  }
+`;
+
+
+const CustomField = styled(Field) `
+  display: none;
+`
+
 
 export default Home
